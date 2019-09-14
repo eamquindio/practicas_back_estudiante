ceb5bda2303c6ad8b4f9f6f63ad1d683e28334b1
Mauricio Ruiz <2320151015@eam.edu.co>
2019-09-14 07:35:41 -0500

    Backen estudiante

--- tests/controllers/StudentControllerTest.js
+++ tests/controllers/StudentControllerTest.js
@@ -1,11 +1,7 @@
-const assert = require('assert');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const app = require('../../index');
-const StudentRepository = require('../../app/repositories/StudentRepository');
 const Helper = require('../Helper');
 
-const API = '/api/estudiantes-ms/students';
 chai.use(chaiHttp);
 
 describe('Student CRUD flows', () => {
@@ -14,54 +10,4 @@ describe('Student CRUD flows', () => {
   beforeEach(async () => {
     await Helper.clear();
   });
-
-  it('find student by name test', async () => {
-    await StudentRepository.create([{
-      id: 1,
-      namee: 'mauricio',
-      lastname: 'ruiz',
-      cellphone: '328732',
-      mail: 'mruiz5363',
-      code: '23452',
-      faculty: 'ingenieria',
-      programm: 'software',
-      semester: '2',
-    },
-    {
-      id: 2,
-      namee: 'manuel',
-      lastname: 'echeverry',
-      cellphone: '754674',
-      mail: 'xternux',
-      code: '65342',
-      faculty: 'administraccion',
-      programm: 'negocios',
-      semester: '5',
-    }]);
-
-    return chai
-      .request(app)
-      .get(`${API}/find_by_name?faculty=ingenieria&&programm=negocios&&semester=7`)
-      .then(async (response) => {
-        const { body } = response;
-        assert.deepEqual(body[0], {
-          id: '1',
-          namee: 'mauricio',
-          lastname: 'ruiz',
-          cellphone: '328732',
-          mail: 'mruiz5363',
-          code: '23452',
-          faculty: 'ingenieria',
-          programm: 'software',
-          semester: '2',
-        });
-      });
-  });
-
-  it('find student by name empty test', async () => chai
-    .request(app)
-    .get(`${API}/find_by_name?faculty=diseno&&programm=negocios&&semester=7`)
-    .then(async (response) => {
-      assert.equal(response.status, 204);
-    }));
 });

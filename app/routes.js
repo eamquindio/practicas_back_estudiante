ff82a790bf91974698d24d2b82dd36c9b3f88e75
Mauricio Ruiz <2320151015@eam.edu.co>
2019-09-14 08:26:46 -0500

    Backen estudiante

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,7 @@ router.put('/persons/:id(\\d+)', PersonController.edit);
 router.get('/persons/find_by_name', PersonController.findByName);
 router.get('/persons/all', PersonController.listAll);
 
-router.get('/student/find_by_name', StudentController.findByName);
+router.get('/student/find_by_namee', StudentController.findByName);
 router.post('/student', StudentController.save);
 router.get('/student/:id(\\d+)', StudentController.find);
 
